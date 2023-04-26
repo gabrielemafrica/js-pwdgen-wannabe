2003e9b22b13b247d21f4b3e49a73777f3c104db
gabrielemafrica <gabriele.ga@live.it>
2023-04-26 15:12:59 +0300

    fine esercizio

---
+++ js/myscript.js
@@ -0,0 +1,56 @@
+
+//crea variabili
+
+let userName;
+let userSurname;
+let color;
+let message;
+let message2;
+
+//informa sulle intenzioni
+
+alert("Adesso scriverai alcuni TUOI DATI.");
+alert("Dopo io GENERERÓ per te una PASSWORD INSICURA");
+alert("CLICCA 'OK' PER PROCEDERE.");
+
+
+//chiedi il nome
+
+userName = prompt("Scrivi qua il tuo NOME.");
+console.log(userName);
+
+//chiedi il cognome
+
+userSurname = prompt("Scrivi qua il tuo COGNOME.");
+console.log(userSurname);
+
+//chiedi colore preferito
+
+color = prompt("Scrivi qua il tuo COLORE PREFERITO.");
+console.log(color);
+
+//informa sul risultato
+
+alert(
+    `
+    Ti chiami ${userName} ${userSurname}.
+    Il tuo colore preferito é ${color}.
+    Prosegui per leggere la PASSWORD.
+    `
+);
+
+//valorizza il messaggio
+
+message =
+    `
+    La PASSWORD INSICURA é
+    <br>
+    <br>
+    ${userName}${userSurname}${color}21
+    `;
+
+
+
+//scrivi password
+
+document.getElementById("mio_id").innerHTML = message;
